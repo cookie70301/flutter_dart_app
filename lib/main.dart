940faf8eb4d8438cd0d67801455a08861b323fc1
Jed Chen <cookie70301@gmail.com>
2025-11-20 14:51:19 +0800

    課堂修改

--- lib/main.dart
+++ lib/main.dart
@@ -29,9 +29,9 @@ class MyHomePage extends StatelessWidget {
                   MaterialPageRoute(
                     builder: (BuildContext context) {
                       return Scaffold(
-                        appBar: AppBar(title: const Text('Next page',style: TextStyle(fontSize: 30),)),
+                        appBar: AppBar(title: const Text('History',style: TextStyle(fontSize: 30),)),
                         body: const Center(
-                        child: Text('這是分頁', style: TextStyle(
+                        child: Text('歷史紀錄之類的', style: TextStyle(
                         fontSize: 24)),
                         ),
                       );
@@ -40,7 +40,7 @@ class MyHomePage extends StatelessWidget {
                 );
               },
               child: Text(
-                "test",style: TextStyle(fontSize: 25)
+                "History",style: TextStyle(fontSize: 25)
               ,)
             ),
             TextButton(
@@ -50,9 +50,9 @@ class MyHomePage extends StatelessWidget {
                       MaterialPageRoute(
                         builder: (BuildContext context) {
                           return Scaffold(
-                            appBar: AppBar(title: const Text('Next page',style: TextStyle(fontSize: 30),)),
+                            appBar: AppBar(title: const Text('About',style: TextStyle(fontSize: 30),)),
                             body: const Center(
-                              child: Text('這是分頁', style: TextStyle(
+                              child: Text('詳情之類的', style: TextStyle(
                                   fontSize: 24)),
                             ),
                           );
@@ -61,17 +61,17 @@ class MyHomePage extends StatelessWidget {
                   );
                 },
                 child: Text(
-                  "test",style: TextStyle(fontSize: 25)
+                  "About",style: TextStyle(fontSize: 25)
                   ,)
             ),
           ],
         ),
       ),
       appBar: AppBar(
-        title: const Text('AppBar Demo'),
+        title: const Text('Home', style: TextStyle(fontSize: 30)),
       ),
       body: const Center(
-          child: Text('This is the home page', style: TextStyle(fontSize: 24))),
+          child: Text('大概是遊戲', style: TextStyle(fontSize: 24))),
     );
   }
 }
